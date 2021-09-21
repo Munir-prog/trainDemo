e35f3535a6938a611e6c5e23aab091a769b2a4e0
Munir Zikrullaev <munirprog@gmail.com>
2021-09-21 21:57:24 +0300

    implement class App

--- src/main/java/com/mprog/App.java
+++ src/main/java/com/mprog/App.java
@@ -1,29 +1,18 @@
 package com.mprog;
 
-import com.mprog.config.ConnectionManager;
-import com.mprog.dao.TicketDao;
-import com.mprog.entity.Ticket;
-import lombok.SneakyThrows;
+import com.mprog.service.ConversationService;
+import com.mprog.service.ConversationServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @ComponentScan
 public class App {
 
-
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(App.class);
-        var ticketDao = context.getBean(TicketDao.class);
-        ticketDao.findAll().forEach(System.out::println);
+        var conversationService = context.getBean(ConversationService.class);
+        conversationService.doConversation();
     }
-
-
-
 }
